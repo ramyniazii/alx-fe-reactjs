26468afcdf2122ba53b3bef91cb4c1da3eaef8be
ramyniazii <ramyniazi14@gmail.com>
2024-09-21 23:04:43 +0300

    Added header, maincontent and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,8 +20,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+       <Header />
       <h1>Vite + React</h1>
        <WelcomeMessage />
+       <MainContent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       <Footer />
     </>
   )
 }
