5b872d7b941de3c8d14153a50f43195b2d878d56
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 00:35:45 +0300

    Tany

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 
 function App() {
   const [count, setCount] = useState(0);
