9684a6563c164ceac9416d130a5c67d5002b727a
ramyniazii <ramyniazi14@gmail.com>
2024-09-21 23:22:57 +0300

    Userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
-import Footer from './components/Footer';
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
       <h1>Vite + React</h1>
        <WelcomeMessage />
        <MainContent />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
