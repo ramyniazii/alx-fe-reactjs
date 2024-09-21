4ba6d0b098314b2b4ce8e616af9462a8ee4497d7
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 00:02:07 +0300

    Counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,7 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import UserProfile from './components/UserProfile';
+import UserProfile from './components/UserProfile'
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,7 @@ function App() {
        <WelcomeMessage />
        <MainContent />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+       <Counter />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
