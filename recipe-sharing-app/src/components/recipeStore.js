619832a1f306ca489b385ad3124088924298048f
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 15:27:11 +0300

    Zustand again

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,8 @@ import create from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
   updateRecipe: (updatedRecipe) => set(state => ({
@@ -9,7 +11,13 @@ const useRecipeStore = create(set => ({
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
 }));
 
 export default useRecipeStore;
