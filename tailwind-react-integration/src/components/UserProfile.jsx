fd29620c91074f6ad401a9c36d6e9d62144c898b
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 17:21:07 +0300

    Styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 md:max-w-sm rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
