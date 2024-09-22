ed1646833e481ca8218f52669a389d7c8f909937
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 16:44:08 +0300

    Comp

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import TodoList from '../TodoList';
+import TodoList from './components/TodoList';
 
 test('renders initial todos', () => {
   render(<TodoList />);
