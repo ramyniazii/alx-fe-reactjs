232f2dee9b09740dc0f666021d0ceaa403fcfb63
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 18:35:24 +0300

    URL

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const BASE_URL = 'https://api.github.com/search/users?q';
 
 export const fetchUserData = async ({ username, location, minRepos }) => {
   try {
