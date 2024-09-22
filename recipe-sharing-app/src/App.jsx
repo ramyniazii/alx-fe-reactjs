02d1a455aa329619024a7e319098cad60c6c2786
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 15:14:15 +0300

    Routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
@@ -11,9 +11,9 @@ const App = () => {
         <h1>Recipe Sharing App</h1>
         <AddRecipeForm />
         <RecipeList />
-        <Switch>
-          <Route path="/recipe/:id" component={RecipeDetails} />
-        </Switch>
+        <Routes>
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
       </div>
     </Router>
   );
