c57b8f089f2c6b933af206dbd1ffe8a7682a9838
ramyniazii <ramyniazi14@gmail.com>
2024-09-22 16:17:56 +0300

    Protectedroute

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
-import BlogPost from './pages/BlogPost'; // Import the BlogPost component
+import BlogPost from './pages/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute'; // Import the ProtectedRoute component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} /> {/* Use ProtectedRoute here */}
         <Route path="/login" element={<Login />} />
-        <Route path="/blog/:id" element={<BlogPost />} /> {/* Add this line */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
